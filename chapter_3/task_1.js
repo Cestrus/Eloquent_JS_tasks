fb345a9b86d1b446ea5b354cf485a033eee5ba8c
unknown <C:\Users\\AppData\Roaming\The Bat!>
2020-11-25 12:58:01 +0200

    have done task_2 chapter_3

---
+++ chapter_3/task_1.js
@@ -0,0 +1,7 @@
+// Напишите функцию min, которая принимает два аргумента и возвращает их минимум.
+
+function min(a, b) {
+  return (a < b)? a : b;
+}
+console.log(min(5, 10));
+console.log(min(15, 4));
